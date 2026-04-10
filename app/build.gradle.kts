87d975904388e15b6c93a83462a4f506967ab12d
Timur <tanharka.tyuchiev4@mail.ru>
2026-04-10 23:21:21 +0500

    chore: bump version to 1.2.1 (versionCode 4)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.animevost.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.2.0"
+        versionCode = 4
+        versionName = "1.2.1"
     }
 
     signingConfigs {
