65dd5528e94467c1e510dfd549b666ff0a6bf745
Timur <tanharka.tyuchiev4@gmail.com>
2026-04-16 05:07:41 +0500

    chore: bump version to 1.6.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.animevost.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 13
-        versionName = "1.5.1"
+        versionCode = 14
+        versionName = "1.6.0"
     }
 
     signingConfigs {
